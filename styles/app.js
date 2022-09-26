74114076fd644d6a59b27eb7092d37d466e7478d
Ayomiposii <ayomiposilasaki@gmail.com>
2022-09-26 11:17:43 +0100

    Implemented the unit conversion

--- styles/app.js
+++ styles/app.js
@@ -31,32 +31,15 @@ function submit(event) {
 let form = document.querySelector("form");
 form.addEventListener("submit", submit);
 
-// for converting degrees
-// function convertCelsius(event) {
-//   event.preventDefault();
-//   let celsiusTemp = document.querySelector("#temp");
-//   celsiusTemp.innerHTML = "28";
-// }
-// function convertFarenheit(event) {
-//   event.preventDefault();
-//   let farenheitTemp = (28 * 9) / 5 + 32;
-//   let celsiusTemp = document.querySelector("#temp");
-//   celsiusTemp.innerHTML = Math.round(farenheitTemp);
-// }
-//
-// let degrees = document.querySelector("#celsius-convert");
-// degrees.addEventListener("click", convertCelsius);
-// let degree = document.querySelector("#fahrenheit-convert");
-// degree.addEventListener("click", convertFarenheit);
-
-// For Temperature
-
 function showWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  // document.querySelector("#temperature").innerHTML = Math.round(
+  //   response.data.main.temp
+  // );
 
+  celsiusTemperature = response.data.main.temp;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   document.querySelector(
     "#humidity"
   ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
@@ -72,7 +55,36 @@ function showWeather(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+} // for converting degrees
+function convertFarenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+function convertCelsius(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null
+let degrees = document.querySelector("#celsius-convert");
+degrees.addEventListener("click", convertCelsius);
+let degree = document.querySelector("#fahrenheit-convert");
+degree.addEventListener("click", convertFarenheit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-convert");
+fahrenheitLink.addEventListener("click", convertFarenheit);
+
+let celsiusLink = document.querySelector("#celsius-convert");
+fahrenheitLink.addEventListener("click", convertCelsius);
+
+// For Temperature
 function searchForCity() {
   let apiKey = "ade2c941f456758f1ba37300092d4328";
   let city = document.querySelector("#search").value;
