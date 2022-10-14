fa2a65bfbe2fd9620a139595855352927433caa7
Ayomiposii <ayomiposilasaki@gmail.com>
2022-10-14 10:47:48 +0100

    Correcting some things

--- styles/app.js
+++ styles/app.js
@@ -29,12 +29,12 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  let forecast = response.data.daily;
+  forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (forecastDay, index) {
+  forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
@@ -69,11 +69,11 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "3499ef150985eccadd080ff408a018df";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
   lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecast);
+  axios.get(`${apiUrl}`).then(displayForecast);
 }
 // for city
 function submit(event) {
